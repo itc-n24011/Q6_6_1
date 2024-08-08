83aec629303691dea68405e83cf45eeca2cf50aa
itc-n24011 <n24011@std.it-college.ac.jp>
2024-08-08 05:51:36 +0000

    Kadai

---
+++ Q6_6_1.py
@@ -0,0 +1,5 @@
+In event1: Happy -> After event1: Happy
+In event2: Sad -> After event2: Happy
+In event3: Tired -> After event3: Tired
+In event4: excite -> After event4: Tired
+In event5: Scare -> After event5: Tired
